e5b569ba6c636b787de2d05aae4a60fe7fbb34e9
Keiffer da Silva <keifferdasilva@gmail.com>
2022-10-17 01:07:39 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("wordSubmit").addEventListener("click", function(event) {
+    event.preventDefault();
+    const value = document.getElementById("wordInput").value;
+    if (value === "")
+        return;
+    console.log(value);
+    let results = "";
+    const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + value;
+    fetch(url)
+    .then(response => response.json())
+    .then(function(json){
+        console.log(json);
+        const wordObject = json[0];
+        results += "<h1>" + wordObject.word.charAt(0).toUpperCase() + wordObject.word.slice(1) + "</h1>";
+        results += "<h2>Definitions</h2>";
+        for(let i = 0; i < wordObject.meanings.length; i++){
+            results += "<h3>Used as a " + wordObject.meanings[i].partOfSpeech + "</h3> <ol>";
+            for(let j = 0; j < wordObject.meanings[i].definitions.length; j++){
+                results += "<li>" + wordObject.meanings[i].definitions[j].definition + "</li>";
+            }
+            results += "</ol>";
+        }
+        document.getElementById("wordResults").innerHTML = results;
+    });
+    
+});
